b0c15bbb8b10d9413eca4d63cb75266ecb61f0f9
Pixako <62527607+Pixako-Unrealistia@users.noreply.github.com>
2024-11-09 21:23:10 +0700

    json -> pl

--- main.py
+++ main.py
@@ -155,6 +155,7 @@ class MapGenerator(QWidget):
 
 		self.tile_sets = []
 		self.load_tile_sets("tile_sets.json")
+		self.json_to_prolog("tile_sets.json", "tile_sets.pl")
 
 		self.initUI()
 
@@ -331,6 +332,26 @@ class MapGenerator(QWidget):
 			print(f"Configuration file {filename} not found. Creating default configuration.")
 			self.tile_sets = self.create_default_tile_sets()
 			self.save_tile_sets(filename)
+
+	def json_to_prolog(self, json_file, prolog_file):
+		with open(json_file, 'r') as f:
+			tile_sets = json.load(f)
+
+		with open(prolog_file, 'w') as f:
+			for tile_set in tile_sets:
+				name = tile_set['name']
+				traversal_cost = tile_set['traversal_cost']
+				cannot_be_next_to = tile_set['cannot_be_next_to']
+				must_be_next_to = tile_set['must_be_next_to']
+				color = tile_set['color']
+				discoverable = 'true' if tile_set['discoverable'] else 'false'
+				texture_path = tile_set['texture_path']
+
+				cannot_be_next_to_str = '[' + ', '.join(cannot_be_next_to) + ']'
+				must_be_next_to_str = '[' + ', '.join(must_be_next_to) + ']'
+
+				fact = f"tile_set({name}, {traversal_cost}, {cannot_be_next_to_str}, {must_be_next_to_str}, '{color}', {discoverable}, '{texture_path}').\n"
+				f.write(fact)
 
 	def display_map(self, map_data):
 		self.scene.clear()
